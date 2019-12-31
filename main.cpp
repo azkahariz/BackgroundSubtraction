ac276716761bcb1a9e59755e362ce916bd42c3bd
azkahariz <azkahariz@gmail.com>
2019-12-31 15:09:06 +0700

    commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,46 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <sstream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
+using namespace cv;
+using namespace std;
 
-    return a.exec();
+int main(){
+    Ptr<BackgroundSubtractor> pBackSub;
+    pBackSub = createBackgroundSubtractorMOG2();
+
+    VideoCapture capture;
+    capture.open(0);
+
+    Mat frame, fgMask;
+    while(true){
+        capture >> frame;
+        GaussianBlur(frame,frame, Size(15,15),0,0);
+        if(frame.empty())
+            break;
+        //update the background model
+        pBackSub->apply(frame, fgMask);
+
+        //get the frame number and write it on the current frame
+        rectangle(frame, cv::Point(10,2), cv::Point(100,20), cv::Scalar(255,255,255), -1);
+        stringstream ss;
+        ss << capture.get(CAP_PROP_POS_FRAMES);
+        string frameNumberString = ss.str();
+        putText(frame, frameNumberString.c_str(), cv::Point(15, 15),
+                FONT_HERSHEY_SIMPLEX, 0.5 , cv::Scalar(0,0,0));
+
+        //show the current frame and the fg masks
+        imshow("Frame", frame);
+        imshow("FG Mask", fgMask);
+        //get the input from the keyboard
+        int keyboard = waitKey(30);
+        if (keyboard == 'q' || keyboard == 27)
+            break;
+    }
+
+    return 0;
 }
-
